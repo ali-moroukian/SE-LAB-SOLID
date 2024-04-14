fab0bb8c23530f912d35683e8dcb89d29bddc3d3
Ehsan200 <ehsan.movaffagh@icloud.com>
2024-04-14 19:08:07 +0330

    feat: Add TelegramMessage

---
+++ src/Step_1_Non_SOLID/models/TelegramMessage.java
@@ -0,0 +1,22 @@
+package Step_1_Non_SOLID.models;
+
+public class TelegramMessage extends Message {
+    private String sourceTelegramId;
+    private String targetTelegramId;
+
+    public String getSourceTelegramId() {
+        return sourceTelegramId;
+    }
+
+    public void setSourceTelegramId(String sourceTelegramId) {
+        this.sourceTelegramId = sourceTelegramId;
+    }
+
+    public String getTargetTelegramId() {
+        return targetTelegramId;
+    }
+
+    public void setTargetTelegramId(String targetTelegramId) {
+        this.targetTelegramId = targetTelegramId;
+    }
+}
