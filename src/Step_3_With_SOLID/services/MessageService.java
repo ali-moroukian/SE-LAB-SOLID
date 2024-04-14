c34386168d504c4fb0b4a29c9950941fef663790
Ehsan200 <ehsan.movaffagh@icloud.com>
2024-04-14 19:18:31 +0330

    chore: Initiate phase3

---
+++ src/Step_3_With_SOLID/services/MessageService.java
@@ -0,0 +1,9 @@
+package Step_3_With_SOLID.services;
+
+import Step_3_With_SOLID.models.EmailMessage;
+import Step_3_With_SOLID.models.SmsMessage;
+
+public interface MessageService {
+    public void sendSmsMessage(SmsMessage smsMessage);
+    public void sendEmailMessage(EmailMessage emailMessage);
+}
